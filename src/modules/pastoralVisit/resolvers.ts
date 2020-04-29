94c3f8037ce9233adfe948bab9607b94092586cf
jan00newbie <positiveKiddo@gmail.com>
2020-04-29 08:37:01 +0200

    resolvers&schema house added

---
+++ src/modules/pastoralVisit/resolvers.ts
@@ -0,0 +1,19 @@
+import { Resolvers } from "../../types/types";
+import PastoralVisit from "../../models/PastoralVisit";
+import User from "../../models/User";
+
+export const resolvers: Resolvers = {
+  PastoralVisit: {
+    id: (pastoralVisit) => pastoralVisit._id + "",
+    priest: async (pastoralVisit) =>
+      User.findOne({ _id: pastoralVisit._id.toHexString() }),
+  },
+  Mutation: {
+    addPastoralVisit: async (_, { input }) => new PastoralVisit(input).save(),
+  },
+  Query: {
+    pastoralVisit: async (_, { input }) =>
+      PastoralVisit.findOne({ _id: input.id }),
+    pastoralVisits: async () => PastoralVisit.find(),
+  },
+};
