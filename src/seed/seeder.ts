3cac31744e7242f3d452468dc0e9a834ce0121be
jan00newbie <positiveKiddo@gmail.com>
2020-07-16 09:47:48 +0200

    consider day in seeder

--- src/seed/seeder.ts
+++ src/seed/seeder.ts
@@ -5,7 +5,7 @@ import loadEnv from "../utils/loadEnv";
 import { createStreetsAndHouses } from "./_housesAndStreets";
 import { createUsers } from "./_users";
 import { createPastoralVisits } from "./_pastoralVisits";
-import { createEntrances } from "./_entrances";
+import { createEntrances, getKeys } from "./_entrances";
 import * as mongoose from "mongoose";
 
 loadEnv();
@@ -19,7 +19,11 @@ loadEnv();
     createStreetsAndHouses(),
   ]);
 
-  const pastoralVisits = await createPastoralVisits(season, users);
+  const { pastoralVisits, days } = await createPastoralVisits(
+    season,
+    users,
+    getKeys(streets) as string[]
+  );
 
   const entrances = await createEntrances(streets, pastoralVisits);
 
